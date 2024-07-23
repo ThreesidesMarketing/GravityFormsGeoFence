9bef7b97eac28a762550162b2d61edb77d83c4da
Mathias Everson <39363442+mathias-threesides@users.noreply.github.com>
2024-07-23 17:08:03 +1000

    Lowercase Country Codes
    Get the first two characters and lowercase compare country codes from IP API.

--- tsm-gform-spam.php
+++ tsm-gform-spam.php
@@ -15,9 +15,8 @@
 class Tsm_Gform_Spam
 {
     // The two letter ISO 3166-1 alpha-2 country codes. (https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2)
-    // Eg. Australia = AU, New Zealand = NZ
-    // To use multiple, separate with commas, ['au', 'nz']
-    protected $Tsm_Gform_Spam_COUNTRY_CODE = ['au', 'AU'];
+    // Use lowercase
+    protected $Tsm_Gform_Spam_COUNTRY_CODE = ['au'];
 
 
     function __construct()
@@ -28,23 +27,25 @@ class Tsm_Gform_Spam
 
     function filter_gform_entry_is_spam_ip_country($is_spam, $form, $entry)
     {
-        if ($is_spam) {
+        if ($is_spam) :
             return $is_spam;
-        }
+        endif;
 
         $ip_address = empty($entry['ip']) ? GFFormsModel::get_ip() : $entry['ip'];
 
-        if (!filter_var($ip_address, FILTER_VALIDATE_IP)) {
+        if (!filter_var($ip_address, FILTER_VALIDATE_IP)) :
             return true;
-        }
+        endif;
 
         $response = wp_remote_get(sprintf('https://ipapi.co/%s/country/', $ip_address));
 
-        if (!is_wp_error($response) && wp_remote_retrieve_response_code($response) === 200) {
+        if (!is_wp_error($response) && wp_remote_retrieve_response_code($response) === 200) :
             $response_country_code = trim(wp_remote_retrieve_body($response));
-
-            return !in_array($response_country_code, $this->Tsm_Gform_Spam_COUNTRY_CODE);
-        }
+ 		    //Lowercase and get two characters
+		    $two_code = strtolower(substr($response_country_code, 0, 2));
+            
+            return !in_array($two_code, $this->Tsm_Gform_Spam_COUNTRY_CODE);
+            endif;
 
         return false;
     }
